33b5e65c88c642cfbcb6b06d76e853341b6b1522
Михаил Кузьмин <misha@Mihails-MacBook-Pro.local>
2021-06-10 17:25:54 +0300

    optimize_1.1

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import tensorflow as tf
 import os
 import numpy as np
-import random
 from skimage.io import imread, imshow
 from skimage.transform import resize
 from skimage import color
@@ -13,7 +12,7 @@ import glob
 img_width = 256
 img_height = 256
 img_channels = 1
-start_neurons = 8
+start_neurons = 16 #(16 - starts to decrease at 60 epoch and should go over 70)
  
 dropout = 0.01
  
@@ -28,8 +27,9 @@ test_path = 'single/test/'
 trainFileNames = glob.glob(train_path + 'images/*')
 testFileNames = glob.glob(test_path + '*')
  
-X_train = np.zeros((len(trainFileNames), img_width, img_height), dtype=np.float)
+X_train = np.zeros((len(trainFileNames), img_width, img_height), dtype = np.float)
 Y_train = np.zeros((len(trainFileNames), img_width, img_height), dtype = np.bool)
+X_test = np.zeros((len(testFileNames), img_height, img_width), dtype = np.float)
 
 ################### load images
 
@@ -42,15 +42,14 @@ for file in glob.glob(train_path + 'images/*'):
     X_train[n] = img
  
     #mask = imread(file[0:36]+ 'masks/rtop/'+ file[43:53]+ '_Rhigh.ome.tiff') # for google drive
-    mask = imread(file[0:13]+ 'masks/Lbot/'+ file[20:30]+ '_Llow.ome.tiff', as_gray=True)
+    mask = imread(file[0:13]+ 'masks/rtop/'+ file[20:30]+ '_Rhigh.ome.tiff', as_gray=True)
     mask = resize(mask, (img_height, img_width), mode='constant', preserve_range=True)
     Y_train[n] = mask
     n+=1
 
-X_test = np.zeros((len(testFileNames), img_height, img_width, img_channels), dtype=np.uint8)
 n = 0
 for file in glob.glob(test_path + '*'):
-    img = imread(file)[:,:,:img_channels]
+    img = color.rgb2gray(imread(file))
     #resize if needed
     img = resize(img, (img_height, img_width), mode='constant', preserve_range=True)
     X_test[n] = img
@@ -138,6 +137,21 @@ results = model.fit(X_train,
                     Y_train,
                     validation_split = 0.2,
                     batch_size = 12,
-                    epochs=10,
+                    epochs=70,
                     callbacks=callbacks)
 
+#model.save('test')
+
+preds_train = model.predict(X_train[:int(X_train.shape[0]*0.9)], verbose=1)
+preds_val = model.predict(X_train[int(X_train.shape[0]*0.9):], verbose=1)
+preds_test = model.predict(X_test, verbose=1)
+
+ix = 1
+
+f, axarr = plt.subplots(1,3) 
+
+axarr[0].imshow(X_train[ix])
+axarr[1].imshow(np.squeeze(Y_train[ix]))
+axarr[2].imshow(np.squeeze(preds_train[ix]))
+
+plt.show()
